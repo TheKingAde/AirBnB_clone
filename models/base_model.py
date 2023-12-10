bb84774c0f911e9563a76a8dbc19e9a398f126e6
ADE <theking-ade@outlook.com>
2023-12-10 13:46:23 -0800

    Revert "fix baseModel and add tests"
    This reverts commit 950e4cabf33cc53b56037b48aa128eaa07f96c36.

--- models/base_model.py
+++ models/base_model.py
@@ -4,46 +4,50 @@ import uuid
 from datetime import datetime
 from collections import OrderedDict
 
-
 class BaseModel:
     """A base class for all other classes"""
 
-    def __init__(self, my_number=None, name=None, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
         """initialize a new instance of BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-        self.my_number = my_number
-        self.name = name
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
 
+
     def __str__(self):
         """Return a string representation of the instance"""
-        O_A = ["my_number", "name", "updated_at", "id", "created_at"]
-        # O_A stands for ordered attribute
+        ordered_attributes = [
+            "my_number",
+            "name",
+            "updated_at",
+            "id",
+            "created_at",
+        ]
+
         attributes_str = ", ".join(
-                "'{}': {!r}".format(attr, getattr(self, attr)) for attr in O_A
+                "'{}': {}".format(attr, getattr(self, attr)) for attr in ordered_attributes
         )
-        return "[{}] ({}) {{{}}}".format(self.__class__.__name__, self.id,
-                                         attributes_str)
+
+        return "[{}] ({}) {{{}}}".format(
+            self.__class__.__name__, self.id, attributes_str
+        )
+
 
     def save(self):
         """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
 
+
     def to_dict(self):
-        """Return a dict representation of the instance with ordered keys"""
-        ordered_keys = [
-            "my_number", "name", "__class__", "updated_at", "id", "created_at"
-        ]
-        result = {
-            key: getattr(self, key)
-            for key in ordered_keys if hasattr(self, key)
-        }
+        """Return a dictionary representation of the instance with ordered keys"""
+        ordered_keys = ["my_number", "name", "__class__", "updated_at", "id", "created_at"]
+        result = {key: getattr(self, key) for key in ordered_keys if hasattr(self, key)}
 
         result['__class__'] = self.__class__.__name__
 
